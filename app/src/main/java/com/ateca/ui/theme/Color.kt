1206cc43c1f25b00ab4697cfd840f37238a938cf
Ленчицкий Станислав Владимирович <st989@ya.ru>
2022-05-16 22:32:48 +0300

    ### Дизайн Внес основные цвета для светлой и темной темы и иконки согласно https://www.figma.com/file/q8zew5zdnQ8V1Um9riKdsZ/Ateca?node-id=51033%3A3536

--- app/src/main/java/com/ateca/ui/theme/Color.kt
+++ app/src/main/java/com/ateca/ui/theme/Color.kt
@@ -2,7 +2,19 @@ package com.ateca.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+//Light Theme
+val White = Color(0xFFFFFFFF) //фон
+val Brown14 = Color(0x14865300) //строка меню
+val Brown = Color(0xFF865300) //иконки в строке меню
+val BlackOlive = Color(0xFF1F1B16) //текст темы карточки
+val MediumGrey = Color(0xFF827568) //окантовка карточки
+val OliveGrey = Color(0xFF504539) //текст карточки
+
+//Dark Theme
+val TransportBlack = Color(0xFF1F1F1F) //фон
+val RedOrange14 = Color(0x14FFB95B) //строка меню
+val RedOrange = Color(0xFFFFB95B) //строка меню
+val GreySilk = Color(0xFFD4C4B5) //текст карточки //иконки в строке меню
+val BlackOliveDark = Color(0xFF1F1B16) //фон карточки
+val Quartz = Color(0xFF9C8E80) //окантовка карточки
+val PearlWhite = Color(0xFFEBE1D9) //текст темы карточки
