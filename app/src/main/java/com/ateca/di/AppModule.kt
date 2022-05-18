11f1886d52033906d4612fbadd393935a10fb3cc
Andrew <shpuntik0902@gmail.com>
2022-05-18 17:39:28 +0300

    Added AppModule

---
+++ app/src/main/java/com/ateca/di/AppModule.kt
@@ -0,0 +1,12 @@
+package com.ateca.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+/**
+ * Created by dronpascal on 18.05.2022.
+ */
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule
