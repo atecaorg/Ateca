20ede0c1ce6ed9c16be81b166635dad2f6f1c8ff
Andrew <shpuntik0902@gmail.com>
2022-05-21 18:20:09 +0300

    Added reusable TopBar components

---
+++ app/src/main/java/com/ateca/ui/components/topappbar/NoteAppTopBar.kt
@@ -0,0 +1,80 @@
+package com.ateca.ui.components.topappbar
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import com.ateca.ui.components.AppPreviewConstants
+import com.ateca.ui.components.AppPreviewConstants.PREVIEW_FONT_SCALE
+import com.ateca.ui.components.AppPreviewConstants.PREVIEW_LIGHT_THEME_BACKGROUND_COLOR
+import com.ateca.ui.components.icon.ThemedIcon
+import com.ateca.ui.theme.md2.AtecaTheme
+
+/**
+ * Created by dronpascal on 20.05.2022.
+ */
+@Composable
+fun NoteAppTopBar(
+    title: String = "",
+    withBackIcon: Boolean = false,
+    onBackPressed: (() -> Unit)? = null,
+    isScrollInInitialState: (() -> Boolean)? = null,
+    actions: @Composable RowScope.() -> Unit = {}
+) {
+
+    val navIcon: @Composable (() -> Unit)? =
+        if (withBackIcon) {
+            {
+                IconButton(
+                    onClick = { onBackPressed?.invoke() }
+                ) {
+                    ThemedIcon(Icons.Filled.ArrowBack, contentDescription = "Back")
+                }
+            }
+        } else {
+            null
+        }
+
+    ScrollAwareTopAppBar(
+        title = {
+            Text(
+                text = title,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        },
+        navigationIcon = navIcon,
+        actions = actions,
+        isScrollInInitialState = isScrollInInitialState
+    )
+}
+
+@Preview(
+    name = "NoteAppTopBarLight",
+    backgroundColor = PREVIEW_LIGHT_THEME_BACKGROUND_COLOR,
+    showBackground = true
+)
+@Preview(
+    name = "NoteAppTopBarDark",
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+)
+@Preview(
+    name = "SearchBarLargeFont",
+    backgroundColor = PREVIEW_LIGHT_THEME_BACKGROUND_COLOR,
+    showBackground = true,
+    fontScale = PREVIEW_FONT_SCALE
+)
+@Composable
+private fun NoteAppTopBarPreview() {
+    AtecaTheme {
+        NoteAppTopBar(
+            withBackIcon = true,
+            title = AppPreviewConstants.title
+        )
+    }
+}
---
+++ app/src/main/java/com/ateca/ui/components/topappbar/ScrollAwareTopAppBar.kt
@@ -0,0 +1,91 @@
+package com.ateca.ui.components.topappbar
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.AppBarDefaults
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.contentColorFor
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.ateca.ui.components.AppPreviewConstants.PREVIEW_LIGHT_THEME_BACKGROUND_COLOR
+import com.ateca.ui.theme.md2.AtecaTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+
+/**
+ * Created by dronpascal on 20.05.2022.
+ */
+@Composable
+fun ScrollAwareTopAppBar(
+    modifier: Modifier = Modifier,
+    title: @Composable () -> Unit = {},
+    navigationIcon: @Composable (() -> Unit)? = null,
+    actions: @Composable RowScope.() -> Unit = {},
+    isScrollInInitialState: (() -> Boolean)? = null,
+) {
+
+    val isScrolledFromIdle: Boolean = isScrollInInitialState?.invoke() == false
+
+    val systemUiController = rememberSystemUiController()
+    val isLightTheme = MaterialTheme.colors.isLight
+    val statusBarColor = if (!isLightTheme && isScrolledFromIdle) {
+        MaterialTheme.colors.surface
+    } else {
+        MaterialTheme.colors.background
+    }
+
+    LaunchedEffect(key1 = isScrolledFromIdle) {
+        systemUiController.setStatusBarColor(
+            color = statusBarColor,
+            darkIcons = isLightTheme
+        )
+    }
+
+    val toolbarElevation: Dp = if (isLightTheme) {
+        if (isScrolledFromIdle) {
+            AppBarDefaults.TopAppBarElevation
+        } else {
+            0.dp
+        }
+    } else {
+        0.dp
+    }
+
+    val backgroundColor: Color = if (!isLightTheme && !isScrolledFromIdle) {
+        MaterialTheme.colors.background
+    } else {
+        MaterialTheme.colors.surface
+    }
+    val contentColor = contentColorFor(backgroundColor)
+
+    TopAppBar(
+        title = title,
+        modifier = modifier,
+        navigationIcon = navigationIcon,
+        actions = actions,
+        backgroundColor = backgroundColor,
+        contentColor = contentColor,
+        elevation = toolbarElevation
+    )
+}
+
+@Preview(
+    name = "ScrollAwareTopAppBarLight",
+    backgroundColor = PREVIEW_LIGHT_THEME_BACKGROUND_COLOR,
+    showBackground = true
+)
+@Preview(
+    name = "ScrollAwareTopAppBarDark",
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+)
+@Composable
+private fun ScrollAwareTopAppBarPreview() {
+    AtecaTheme {
+        ScrollAwareTopAppBar()
+    }
+}
