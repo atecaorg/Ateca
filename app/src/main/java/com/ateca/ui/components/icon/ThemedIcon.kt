0dc78f7fc1ac3bdb2fe46a3ee96f1f4a9dad6fec
Andrew <shpuntik0902@gmail.com>
2022-05-21 18:19:22 +0300

    Added reusable Icon components

---
+++ app/src/main/java/com/ateca/ui/components/icon/ThemedIcon.kt
@@ -0,0 +1,64 @@
+package com.ateca.ui.components.icon
+
+import android.content.res.Configuration
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import com.ateca.ui.components.AppPreviewConstants
+import com.ateca.ui.components.AppPreviewConstants.PREVIEW_LIGHT_THEME_BACKGROUND_COLOR
+import com.ateca.ui.theme.md2.AtecaTheme
+
+/**
+ * Created by dronpascal on 20.05.2022.
+ */
+@Composable
+fun ThemedIcon(
+    imageVector: ImageVector,
+    contentDescription: String?,
+    modifier: Modifier = Modifier,
+    tint: Color = MaterialTheme.colors.onSurface
+) {
+    Icon(imageVector, contentDescription, modifier, tint)
+}
+
+@Composable
+fun ThemedIcon(
+    painter: Painter,
+    contentDescription: String?,
+    modifier: Modifier = Modifier,
+    tint: Color = MaterialTheme.colors.onSurface
+) {
+    Icon(painter, contentDescription, modifier, tint)
+}
+
+@Preview(
+    name = "ThemedIconLight",
+    backgroundColor = PREVIEW_LIGHT_THEME_BACKGROUND_COLOR,
+    showBackground = true
+)
+@Preview(
+    name = "ThemedIconDark",
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+)
+@Preview(
+    name = "StubScreenLargeFont",
+    fontScale = AppPreviewConstants.PREVIEW_FONT_SCALE,
+    backgroundColor = PREVIEW_LIGHT_THEME_BACKGROUND_COLOR,
+    showBackground = true
+)
+@Composable
+private fun ThemedIconPreview() {
+    AtecaTheme {
+        ThemedIcon(
+            imageVector = Icons.Filled.ArrowBack,
+            contentDescription = "Back"
+        )
+    }
+}
