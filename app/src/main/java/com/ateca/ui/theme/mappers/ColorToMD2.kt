20209be6d973604811f9967f64054d403dd05529
Ленчицкий Станислав Владимирович <st989@ya.ru>
2022-05-23 12:48:45 +0300

    Вынес маппер ColorScheme.toMD2 в отдельный файл

---
+++ app/src/main/java/com/ateca/ui/theme/mappers/ColorToMD2.kt
@@ -0,0 +1,21 @@
+package com.ateca.ui.theme.mappers
+
+import androidx.compose.material.Colors
+import androidx.compose.material3.ColorScheme
+import androidx.compose.ui.graphics.luminance
+
+fun ColorScheme.toMD2 () = Colors(
+    primary = this.primary,
+    primaryVariant = this.primaryContainer,
+    secondary = this.secondary,
+    secondaryVariant= this.secondaryContainer,
+    background= this.background,
+    surface = this.surface,
+    error = this.error,
+    onPrimary = this.onPrimary,
+    onSecondary= this.onSecondary,
+    onBackground = this.onBackground,
+    onSurface = this.onSurface,
+    onError = this.onError,
+    isLight = this.background.luminance() > 0.5
+)
