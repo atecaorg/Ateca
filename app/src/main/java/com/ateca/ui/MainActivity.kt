09c60f8ecd07a31f35e36b18cb7c561dd37247ef
Podzorov Eugene <p.eugenics@gmail.com>
2022-05-26 11:14:12 +0700

    refactored

--- app/src/main/java/com/ateca/ui/MainActivity.kt
+++ app/src/main/java/com/ateca/ui/MainActivity.kt
@@ -4,27 +4,40 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import com.ateca.UserSettings
+import com.ateca.domain.datasource.ISettingsDataSource
 import com.ateca.ui.components.app.AtecaApp
 import com.ateca.ui.theme.md2.AtecaTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    private val viewModel: MainViewModel by viewModels()
+    @Inject
+    lateinit var dataStore: ISettingsDataSource
+    private val settings = MutableStateFlow<UserSettings>(UserSettings.getDefaultInstance())
 
     @SuppressLint("UnrememberedMutableState")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        CoroutineScope(Dispatchers.IO).launch {
+            dataStore.getSetting().collect {
+                settings.value = it
+            }
+        }
+
         setContent {
-            val composeTheme = viewModel.settings.collectAsState().value.theme
+            val composeTheme = settings.collectAsState().value.theme
             val isDark = mutableStateOf(
                 when (composeTheme) {
                     UserSettings.Theme.DARK -> true
