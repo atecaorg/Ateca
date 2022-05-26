baa9008088924e8420e3655d8fb925c560113a41
Podzorov Eugene <p.eugenics@gmail.com>
2022-05-26 14:38:42 +0700

    Revert "refactored"
    This reverts commit 5cf72044c7dcc5bbf99694eadfa230c3da91e23f.

--- app/src/main/java/com/ateca/ui/MainActivity.kt
+++ app/src/main/java/com/ateca/ui/MainActivity.kt
@@ -4,40 +4,27 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import com.ateca.UserSettings
-import com.ateca.domain.datasource.ISettingsDataSource
 import com.ateca.ui.components.app.AtecaApp
 import com.ateca.ui.theme.md2.AtecaTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    @Inject
-    lateinit var dataStore: ISettingsDataSource
-    private val settings = MutableStateFlow<UserSettings>(UserSettings.getDefaultInstance())
+    private val viewModel: MainViewModel by viewModels()
 
     @SuppressLint("UnrememberedMutableState")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        CoroutineScope(Dispatchers.IO).launch {
-            dataStore.getSetting().collect {
-                settings.value = it
-            }
-        }
-
         setContent {
-            val composeTheme = settings.collectAsState().value.theme
+            val composeTheme = viewModel.settings.collectAsState().value.theme
             val isDark = mutableStateOf(
                 when (composeTheme) {
                     UserSettings.Theme.DARK -> true
