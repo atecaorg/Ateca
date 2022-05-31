eba6f67fcff9be8c42c92d4e577172659ebdab69
Andrew <shpuntik0902@gmail.com>
2022-05-31 13:33:57 +0300

    Added search constants
    - Debounce time
    - Empty search query

---
+++ app/src/main/java/com/ateca/domain/constants/SearchConstants.kt
@@ -0,0 +1,10 @@
+package com.ateca.domain.constants
+
+/**
+ * Created by dronpascal on 31.05.2022.
+ */
+object SearchConstants {
+
+    const val EMPTY_QUERY = ""
+    const val DEBOUNCE_DELAY_TIME_MS = 300L
+}
