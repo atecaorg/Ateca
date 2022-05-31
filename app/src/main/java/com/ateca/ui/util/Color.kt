78224bcee004499387ce5b32fb534a71ed984abc
Andrew <shpuntik0902@gmail.com>
2022-05-31 17:46:06 +0300

    Added annotation to color util function parameter

--- app/src/main/java/com/ateca/ui/util/Color.kt
+++ app/src/main/java/com/ateca/ui/util/Color.kt
@@ -1,11 +1,15 @@
 package com.ateca.ui.util
 
+import androidx.annotation.FloatRange
 import androidx.compose.ui.graphics.Color
 
 /**
  * Created by dronpascal on 24.05.2022.
  */
-internal fun Color.darker(factor: Float): Color {
+internal fun Color.darker(
+    @FloatRange(from = 0.0, to = 1.0)
+    factor: Float
+): Color {
     return Color(
         alpha = this.alpha,
         red = this.red * factor,
