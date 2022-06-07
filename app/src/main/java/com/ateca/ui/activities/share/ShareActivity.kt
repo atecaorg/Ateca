d4fa3bc97851f09b3db917c4a246e7edea3c38c4
Podzorov Eugene <p.eugenics@gmail.com>
2022-06-07 15:23:40 +0700

    share text data in ateca

---
+++ app/src/main/java/com/ateca/ui/activities/share/ShareActivity.kt
@@ -0,0 +1,141 @@
+package com.ateca.ui.activities.share
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.ateca.domain.datasource.ISettingsDataSource
+import com.ateca.domain.entity.Theme
+import com.ateca.domain.interactors.NoteInteractors
+import com.ateca.domain.models.ApplicationSettings
+import com.ateca.domain.models.Note
+import com.ateca.ui.theme.md2.AtecaTheme
+import com.ateca.ui.theme.spacing
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+import com.ateca.R
+
+@AndroidEntryPoint
+class ShareActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var dataStore: ISettingsDataSource
+
+    @Inject
+    lateinit var noteInteractors: NoteInteractors
+
+    private val settings = MutableStateFlow(ApplicationSettings())
+    private val scope = CoroutineScope(Dispatchers.IO)
+
+    @SuppressLint("UnrememberedMutableState")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val intentTextData = getTextIntentData()
+
+        if (intentTextData.isNotEmpty()) {
+            scope.launch {
+                dataStore.getSetting().collect {
+                    settings.value = it
+                }
+            }
+
+            val note = Note(
+                title = intentTextData[SUBJECT_KEY] ?: EMPTY_STRING,
+                text = intentTextData[TEXT_KEY] ?: EMPTY_STRING
+            )
+
+            setContent {
+                val composeTheme = settings.collectAsState().value.theme
+                val isDark = mutableStateOf(
+                    when (composeTheme) {
+                        Theme.DARK -> true
+                        Theme.LIGHT -> false
+                        else -> isSystemInDarkTheme()
+                    }
+                )
+
+                AtecaTheme(isDarkTheme = isDark.value) {
+                    ShareDialog(
+                        title = note.title,
+                        text = note.text
+                    ) {
+                        scope.launch {
+                            noteInteractors.saveNote.execute(note).collect {}
+                        }
+                        Toast.makeText(
+                            this,
+                            getString(R.string.note_saved),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        finish()
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    private fun ShareDialog(
+        title: String,
+        text: String,
+        onSaveShare: () -> Unit
+    ) {
+        AlertDialog(
+            shape = RoundedCornerShape(16.dp),
+            onDismissRequest = { finish() },
+            confirmButton = {
+                TextButton(onClick = onSaveShare) {
+                    Text(text = stringResource(R.string.save))
+                }
+            },
+            dismissButton = {},
+            title = {
+                Column {
+                    Text(
+                        text = title,
+                        style = MaterialTheme.typography.h6
+                    )
+                    Divider(
+                        thickness = 1.dp,
+                        modifier = Modifier.padding(MaterialTheme.spacing.small)
+                    )
+                }
+            },
+            text = { Text(text = text) }
+        )
+    }
+
+    private fun getTextIntentData(): Map<String, String> {
+        val intentTextData = mutableMapOf<String, String>()
+        if (intent.hasExtra(Intent.EXTRA_TEXT)) {
+            intentTextData[SUBJECT_KEY] =
+                intent.getStringExtra(Intent.EXTRA_SUBJECT) ?: EMPTY_STRING
+            intentTextData[TEXT_KEY] = intent.getStringExtra(Intent.EXTRA_TEXT) ?: EMPTY_STRING
+        }
+        return intentTextData
+    }
+
+    companion object {
+        private const val SUBJECT_KEY = "SUBJECT"
+        private const val TEXT_KEY = "TEXT"
+        private const val EMPTY_STRING = ""
+    }
+}
