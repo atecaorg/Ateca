81eb9516cde9eefc12719bb5dc9e711a1491c6ea
Andrew <shpuntik0902@gmail.com>
2022-06-09 12:01:08 +0300

    Added constants

---
+++ app/src/main/java/com/ateca/ui/screens/note_screen/view/constants/MDTags.kt
@@ -0,0 +1,9 @@
+package com.ateca.ui.screens.note_screen.view.constants
+
+/**
+ * Created by dronpascal on 09.06.2022.
+ */
+object MDTags {
+    const val TAG_URL = "url"
+    const val TAG_IMAGE_URL = "imageUrl"
+}
